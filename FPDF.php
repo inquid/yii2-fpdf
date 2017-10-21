3316063fdb309d3dc6dcc0a210b4d02893950795
Luis Gonzalez <luisarmando1234@gmail.com>
2017-10-21 15:19:15 -0500

    Update FPDF.php

--- FPDF.php
+++ FPDF.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace integready\pdf;
+namespace inquid\pdf;
 
 /*******************************************************************************
  * FPDF                                                                         *
